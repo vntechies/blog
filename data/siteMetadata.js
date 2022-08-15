d3eb81d139a25ee687908a4d0883912f9ed206bc
Mau <mau@qmau.me>
2022-08-15 20:42:48 +0900

    add default ogp

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/vntechies/blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/default-ogp.png',
   email: 'info@vntechies.com',
   github: 'https://github.com/vntechies',
   twitter: 'https://twitter.com/vntechies',
