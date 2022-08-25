c2fed66090873730f1638be1417f4211e5b7fa0b
Mau <mau@qmau.me>
2022-08-26 01:00:36 +0900

    add blog post aws saa c03

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,6 @@ export default function Home({ posts }) {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             🌟 Bài viết mới nhất
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
