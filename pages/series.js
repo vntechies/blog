7ddcb79a7a3178221de0373d6aee890d2ccc3e41
Mau <mau@qmau.me>
2022-10-20 15:06:43 +0900

    content: added 90daysofdevops day 11 content

--- pages/series.js
+++ pages/series.js
@@ -11,7 +11,10 @@ export async function getStaticProps() {
 export default function Series({ series }) {
   return (
     <>
-      <PageSEO title={`Series - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <PageSEO
+        title={`Series - ${siteMetadata.author}`}
+        description={`Series - ${siteMetadata.description}`}
+      />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
