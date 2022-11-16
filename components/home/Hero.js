5c06987dec4ee9dc9eaa968e3e56215a709bf017
Mau <mau@qmau.me>
2022-11-17 01:23:05 +0900

    layout: fixed index hero

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -16,7 +16,7 @@ const Hero = () => {
           <div className="flex flex-col items-start space-y-3 sm:flex-row sm:items-center sm:space-x-4 sm:space-y-0">
             <Link
               href="/blog"
-              className="mr-3 inline-flex items-center justify-center rounded-lg bg-primary-400 px-5 py-3 text-center text-base font-medium focus:ring-4 focus:ring-primary-300 hover:bg-primary-800 dark:bg-primary-700 dark:focus:ring-primary-900"
+              className="mr-1 inline-flex items-center justify-center rounded-lg bg-primary-400 px-5 py-3 text-center text-base font-medium focus:ring-4 focus:ring-primary-300 hover:bg-primary-800 dark:bg-primary-700 dark:focus:ring-primary-900"
             >
               Đọc bài ngay!
               <svg
@@ -31,6 +31,29 @@ const Hero = () => {
                   clipRule="evenodd"
                 ></path>
               </svg>
+            </Link>
+            <Link
+              href="https://facebook.com/vntechies"
+              arial="VNTechies Page"
+              type="button"
+              className="dark:focus:ring-[#3b5998]/55 mr-3 inline-flex items-center justify-center rounded-lg bg-[#3b5998] px-5 py-3 text-center text-base font-medium focus:ring-4 focus:ring-4 focus:ring-primary-300 focus:ring-[#3b5998]/50 hover:bg-[#3b5998]/90"
+            >
+              <svg
+                className="mr-2 -ml-1 h-4 w-4"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="facebook-f"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z"
+                ></path>
+              </svg>
+              Facebook Page
             </Link>
             <Link
               href="https://github.com/vntechies/blog"
@@ -50,7 +73,7 @@ const Hero = () => {
                 <title>GitHub</title>
                 <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
               </svg>
-              <span> Xem trên Github</span>
+              <span>vntechies/blog</span>
             </Link>
           </div>
         </div>
